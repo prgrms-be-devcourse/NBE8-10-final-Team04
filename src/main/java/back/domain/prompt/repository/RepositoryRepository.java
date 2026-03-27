209e46901f6a8745a83573687a1bdcfa4263a3ec
kimhss <hyunuary108@kyonggi.ac.kr>
2026-03-27 22:57:15 +0900

    [REFACTOR/TM-120] repository 필요없는 함수 제거

--- src/main/java/back/domain/prompt/repository/RepositoryRepository.java
+++ src/main/java/back/domain/prompt/repository/RepositoryRepository.java
@@ -8,12 +8,7 @@ import java.util.Optional;
 
 public interface RepositoryRepository extends JpaRepository<Repository, Long> {
 
-    // github_id 기준 중복 체크
-    boolean existsByGithubId(Long githubId);
-
     // github_id로 조회
     Optional<Repository> findByGithubId(Long githubId);
 
-    // source_repo로 조회 ("octocat/my-repo")
-    Optional<Repository> findBySourceRepo(String sourceRepo);
 }
