5839fa71c7978f316cd845643a561f89ec0afe3b
kimhss <hyunuary108@kyonggi.ac.kr>
2026-03-27 22:21:33 +0900

    [FEAT/TM-119] repository 안 쓰는 함수 제거 및 함수 추가

--- src/main/java/back/domain/info/repository/AiModelFamilyRepository.java
+++ src/main/java/back/domain/info/repository/AiModelFamilyRepository.java
@@ -11,23 +11,4 @@ import java.util.Optional;
 @Repository
 public interface AiModelFamilyRepository extends JpaRepository<AiModelFamily, Integer> {
 
-    // 특정 벤더의 모델 패밀리 전체 조회
-    List<AiModelFamily> findByVendorId(Long vendorId);
-
-    // 패밀리명으로 조회
-    Optional<AiModelFamily> findByFamilyName(String familyName);
-
-    // 패밀리명 부분 검색 (대소문자 무시)
-    List<AiModelFamily> findByFamilyNameContainingIgnoreCase(String keyword);
-
-    // 특정 벤더의 패밀리명으로 조회
-    Optional<AiModelFamily> findByVendorIdAndFamilyName(Long vendorId, String familyName);
-
-    // 모델 목록을 함께 fetch join으로 조회
-    @Query("SELECT f FROM AiModelFamily f LEFT JOIN FETCH f.models WHERE f.id = :id")
-    Optional<AiModelFamily> findByIdWithModels(Integer id);
-
-    // 벤더 정보까지 함께 fetch join으로 조회
-    @Query("SELECT f FROM AiModelFamily f JOIN FETCH f.vendor WHERE f.id = :id")
-    Optional<AiModelFamily> findByIdWithVendor(Integer id);
 }
--- src/main/java/back/domain/info/repository/CategoryStatRepository.java
+++ src/main/java/back/domain/info/repository/CategoryStatRepository.java
@@ -3,5 +3,8 @@ package back.domain.info.repository;
 import back.domain.info.entity.CategoryStat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryStatRepository extends JpaRepository<CategoryStat, Long> {
+    Optional<CategoryStat> findByCategory(String category);
 }
