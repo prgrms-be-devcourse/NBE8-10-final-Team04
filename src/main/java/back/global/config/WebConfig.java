5b211144cbd6266617b0b6f33d8636329711f71a
kimhss <hyunuary108@kyonggi.ac.kr>
2026-03-30 00:06:49 +0900

    [FEAT/TM-128] WebConfig 임베딩 baseUrl @Bean 추가

--- src/main/java/back/global/config/WebConfig.java
+++ src/main/java/back/global/config/WebConfig.java
@@ -1,7 +1,9 @@
 package back.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,5 +25,12 @@ public class WebConfig implements WebMvcConfigurer {
 
         // 업로드된 파일을 정적 리소스로 제공
         registry.addResourceHandler("/uploads/**").addResourceLocations("file:" + uploadDir + "/");
+    }
+
+    @Bean
+    public WebClient embeddingWebClient() {
+        return WebClient.builder()
+                .baseUrl("http://your-embedding-service:8001")
+                .build();
     }
 }
