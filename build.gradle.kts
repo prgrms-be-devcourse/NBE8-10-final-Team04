2eafaaef6fd065a933332cad776b4523325ed9d5
sia <piao516@naver.com>
2026-04-06 18:35:41 +0900

    [FIX/TM-103] 파일체인지 수정
    fix
    
    test
    
    [FIX/TM-103] p2 피드백 수정 ã완료

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     runtimeOnly("com.h2database:h2")
-    runtimeOnly("com.mysql:mysql-connector-j")
+//    runtimeOnly("com.mysql:mysql-connector-j") 의존성제거
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-restdocs")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
