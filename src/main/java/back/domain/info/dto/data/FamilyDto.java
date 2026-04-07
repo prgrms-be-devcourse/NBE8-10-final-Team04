a45c525e9896fc8065642db2d7391b2f7449c895
kimhss <hyunuary108@kyonggi.ac.kr>
2026-04-07 11:20:12 +0900

    [FEAT/TM-185] @SpotBugs FamilyDto에 방어적 복사 추가

--- src/main/java/back/domain/info/dto/data/FamilyDto.java
+++ src/main/java/back/domain/info/dto/data/FamilyDto.java
@@ -15,4 +15,18 @@ public record FamilyDto(
         @JsonProperty("output_types")
         String[] outputTypes
 ) {
+    public FamilyDto {
+        inputTypes = inputTypes == null ? null : inputTypes.clone();
+        outputTypes = outputTypes == null ? null : outputTypes.clone();
+    }
+
+    @Override
+    public String[] inputTypes() {
+        return inputTypes == null ? null : inputTypes.clone();
+    }
+
+    @Override
+    public String[] outputTypes() {
+        return outputTypes == null ? null : outputTypes.clone();
+    }
 }
