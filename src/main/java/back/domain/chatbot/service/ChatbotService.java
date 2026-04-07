cc774a4b9a64b1d6efe668694c1255594875dcc1
sia <piao516@naver.com>
2026-04-08 00:29:00 +0900

    [FEAT/TM-109] 의ì¡íì스팟버그 수정

--- src/main/java/back/domain/chatbot/service/ChatbotService.java
+++ src/main/java/back/domain/chatbot/service/ChatbotService.java
@@ -2,12 +2,13 @@ package back.domain.chatbot.service;
 
 import back.domain.chatbot.dto.request.ChatbotRequest;
 import back.domain.chatbot.dto.response.ChatbotResponse;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@SuppressWarnings("EI_EXPOSE_REP2")
+@SuppressFBWarnings("EI_EXPOSE_REP2")
 @Service
 @RequiredArgsConstructor
 public class ChatbotService {
