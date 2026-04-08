5b42d11951df00571f6b3908e87dd4fcc89c4db3
sia <piao516@naver.com>
2026-04-09 05:12:08 +0900

    [DELETE/TM-159FIXì 스팟버그 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -133,7 +133,9 @@ tasks.named<JacocoCoverageVerification>("jacocoTestCoverageVerification") {
             excludes = listOf(
                 "**.*DTO*",        // 이름에 DTO가 포함된 모든 클래스 제외
                 "**.config.**",     // config 패키지 안에 있는 모든 클래스 제외
-                "**.*Application*"  // 메인 실행 클래스 제외
+                "**.*Application*",  // 메인 실행 클래스 제외
+                "back.domain.payment.dto.response.*",
+                "back.domain.payment.client.*"
             )
             limit {
                 counter = "LINE"
